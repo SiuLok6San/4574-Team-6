4c0adbd4793aa2ff20275fc5351e4e4861592cdf
Yuqing Tao <tyq20021103@gmail.com>
2026-04-18 00:30:36 +0000

    Update on page views and sessions

--- models/BASE/WEB_PAGE_VIEWS.sql
+++ models/BASE/WEB_PAGE_VIEWS.sql
@@ -2,7 +2,7 @@ select
     page_name,
     session_id,
     view_at,
-    _fivetran_deleted,
-    _fivetran_id,
-    _fivetran_synced
+    "_fivetran_deleted" as _fivetran_deleted,
+    "_fivetran_id" as _fivetran_id,
+    "_fivetran_synced" as _fivetran_synced
 from {{ source('web_schema', 'PAGE_VIEWS') }}
