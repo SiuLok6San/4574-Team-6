1556145dbc5fa42d53517a0093b65fe0a61263f7
Yuechen Xiao <siuloksan@gmail.com>
2026-04-18 00:32:11 +0000

    Update on Item views and orders

--- models/BASE/WEB_ITEM_VIEWS.sql
+++ models/BASE/WEB_ITEM_VIEWS.sql
@@ -5,7 +5,7 @@ select
     price_per_unit,
     remove_from_cart_quantity,
     session_id,
-    _fivetran_deleted,
-    _fivetran_id,
-    _fivetran_synced
+    "_fivetran_deleted" as _fivetran_deleted,
+    "_fivetran_id" as _fivetran_id,
+    "_fivetran_synced" as _fivetran_synced
 from {{ source("web_schema", "ITEM_VIEWS") }}
