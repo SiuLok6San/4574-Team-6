fb102564de678c7b548c2d22412544d7f9f1166a
Haodong Yan <yhd10032@gmail.com>
2026-04-19 20:07:29 +0000

    added quotes for Fivetran fields

--- models/BASE/WEB_ITEM_VIEWS.sql
+++ models/BASE/WEB_ITEM_VIEWS.sql
@@ -5,7 +5,7 @@ select
     price_per_unit,
     remove_from_cart_quantity,
     session_id,
-    _fivetran_deleted,
-    _fivetran_id,
-    _fivetran_synced
+    "_fivetran_deleted",
+    "_fivetran_id",
+    "_fivetran_synced"
 from {{ source("web_schema", "ITEM_VIEWS") }}
--- models/BASE/WEB_PAGE_VIEWS.sql
+++ models/BASE/WEB_PAGE_VIEWS.sql
@@ -2,7 +2,7 @@ select
     page_name,
     session_id,
     view_at,
-    _fivetran_deleted,
-    _fivetran_id,
-    _fivetran_synced
-from {{ source('web_schema', 'PAGE_VIEWS') }}
+    "_fivetran_deleted",
+    "_fivetran_id",
+    "_fivetran_synced"
+from {{ source("web_schema", "PAGE_VIEWS") }}
