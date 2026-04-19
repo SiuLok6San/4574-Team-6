46a7cb5e2f0de3515cf6a410abf8a2c77f21136c
Haodong Yan <yhd10032@gmail.com>
2026-04-19 19:48:19 +0000

    fix web_sessions fivetran fields

--- models/BASE/WEB_SESSIONS.sql
+++ models/BASE/WEB_SESSIONS.sql
@@ -4,7 +4,7 @@ select
     os,
     session_at,
     session_id,
-    _fivetran_deleted,
-    _fivetran_id,
-    _fivetran_synced
+    "_fivetran_id",
+    "_fivetran_deleted",
+    "_fivetran_synced"
 from {{ source('web_schema', 'SESSIONS') }}
