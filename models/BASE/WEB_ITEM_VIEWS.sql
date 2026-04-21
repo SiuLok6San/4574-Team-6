cb07f80adf7047e6ca915172de94c22249839f80
Yuechen Xiao <siuloksan@gmail.com>
2026-04-21 03:50:38 +0000

    Created schema.yml and tested on dbt

--- models/BASE/WEB_ITEM_VIEWS.sql
+++ models/BASE/WEB_ITEM_VIEWS.sql
@@ -5,13 +5,7 @@ select
     price_per_unit,
     remove_from_cart_quantity,
     session_id,
-<<<<<<< HEAD
-    "_fivetran_deleted",
-    "_fivetran_id",
-    "_fivetran_synced"
-=======
     "_fivetran_deleted" as _fivetran_deleted,
     "_fivetran_id" as _fivetran_id,
     "_fivetran_synced" as _fivetran_synced
->>>>>>> dd294406af7b40420bfffad475cd3bfb05c0d8a0
-from {{ source("web_schema", "ITEM_VIEWS") }}
+from {{ source('web_schema', 'ITEM_VIEWS') }}
--- models/BASE/WEB_SESSIONS.sql
+++ models/BASE/WEB_SESSIONS.sql
@@ -2,15 +2,9 @@ select
     client_id,
     ip,
     os,
-    session_at,
+    try_to_timestamp(session_at) as session_at,
     session_id,
-<<<<<<< HEAD
-    "_fivetran_id",
-    "_fivetran_deleted",
-    "_fivetran_synced"
-=======
     "_fivetran_deleted" as _fivetran_deleted,
     "_fivetran_id" as _fivetran_id,
     "_fivetran_synced" as _fivetran_synced
->>>>>>> dd294406af7b40420bfffad475cd3bfb05c0d8a0
 from {{ source('web_schema', 'SESSIONS') }}
