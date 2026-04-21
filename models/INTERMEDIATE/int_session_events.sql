7e9c71870ed0ddc79dabd9f3a44fdd8d99daf504
Yuechen Xiao <siuloksan@gmail.com>
2026-04-21 18:48:23 +0000

    Fix the fact_seession error

--- models/INTERMEDIATE/int_session_events.sql
+++ models/INTERMEDIATE/int_session_events.sql
@@ -7,7 +7,6 @@ with page_views as (
         max(case when page_name = 'cart' then 1 else 0 end) as viewed_cart,
         min(view_at) as first_page_view_at
     from {{ ref('WEB_PAGE_VIEWS') }}
-    where session_id is not null
     group by 1
 
 ),
@@ -17,10 +16,10 @@ item_views as (
     select
         session_id,
         max(1) as viewed_item,
+        count(*) as item_view_count,
         sum(coalesce(add_to_cart_quantity, 0)) as total_add_to_cart_qty,
         sum(coalesce(remove_from_cart_quantity, 0)) as total_remove_from_cart_qty
     from {{ ref('WEB_ITEM_VIEWS') }}
-    where session_id is not null
     group by 1
 
 ),
@@ -32,36 +31,24 @@ orders as (
         max(1) as placed_order,
         count(*) as num_orders
     from {{ ref('int_orders') }}
-    where session_id is not null
     group by 1
-
-),
-
-all_sessions as (
-
-    select session_id from page_views
-    union
-    select session_id from item_views
-    union
-    select session_id from orders
 
 )
 
 select
-    s.session_id,
+    coalesce(p.session_id, i.session_id, o.session_id) as session_id,
     coalesce(p.viewed_landing_page, 0) as viewed_landing_page,
     coalesce(p.viewed_shop_plants, 0) as viewed_shop_plants,
     coalesce(p.viewed_cart, 0) as viewed_cart,
     coalesce(i.viewed_item, 0) as viewed_item,
+    coalesce(i.item_view_count, 0) as item_view_count,
     coalesce(i.total_add_to_cart_qty, 0) as total_add_to_cart_qty,
     coalesce(i.total_remove_from_cart_qty, 0) as total_remove_from_cart_qty,
     coalesce(o.placed_order, 0) as placed_order,
     coalesce(o.num_orders, 0) as num_orders,
     p.first_page_view_at
-from all_sessions s
-left join page_views p
-    on s.session_id = p.session_id
-left join item_views i
-    on s.session_id = i.session_id
-left join orders o
-    on s.session_id = o.session_id
+from page_views p
+full outer join item_views i
+    on p.session_id = i.session_id
+full outer join orders o
+    on coalesce(p.session_id, i.session_id) = o.session_id
