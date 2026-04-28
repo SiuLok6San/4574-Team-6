78e25e15257d79029d106243c08990989f59f4d9
Yuechen Xiao <siuloksan@gmail.com>
2026-04-28 17:03:41 +0000

    solved error

--- models/ANALYTICS/fact_order.sql
+++ models/ANALYTICS/fact_order.sql
@@ -28,23 +28,5 @@ select
     coalesce(r.refunded_flag, 0) as refunded_flag,
     coalesce(r.return_record_count, 0) as return_record_count
 from {{ ref('int_orders') }} o
-    left join returns r
+left join returns r
     on o.order_id = r.order_id
-    order_id,
-    session_id,
-    order_at,
-    client_name,
-    payment_method,
-    shipping_address,
-    shipping_cost,
-    phone,
-    state,
-    tax_rate,
-    first_returned_at,
-    refunded_flag,
-    return_record_count
-    item_revenue, 
-    order_revenue,  
-    total_revenue
-from {{ ref('int_order_returns') }}
-
